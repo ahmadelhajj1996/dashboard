bac429d16d960f387036279d8285d8630e07af0b
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2026-05-19 09:18:40 -0700

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,9 @@ import { store } from "../store/store";
 
 
 const api = axios.create({
-  baseURL: "https://phplaravel-1626350-6427540.cloudwaysapps.com/api/",
+  // baseURL: "https://phplaravel-1626350-6427540.cloudwaysapps.com/api/",
+      baseURL: "http://127.0.0.1:8000/api/",
+
   headers: {
     "Content-Type": "multipart/form-data",
   },

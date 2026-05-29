f82ba28425f0051ab6f87c6b1dd226d6f7c40a91
Ahmad Elhaj <ahmad.1996.elhajl@gmail.com>
2026-05-28 18:57:44 -0700

    second upload

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-
-const api = axios.create({          
-      baseURL: "http://127.0.0.1:8000/api/",
-  // baseURL: "https://phplaravel-1626350-6427540.cloudwaysapps.com/api/",
-headers: {
-  "Content-Type": "multipart/form-data",
-},
+const api = axios.create({
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://phplaravel-1626350-6427540.cloudwaysapps.com/api/",
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
   timeout: 10000,
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
